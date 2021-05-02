85b5f9d57c48273ce64cb772750e04ae6c2253f6
Jordy Yeoman <Jordyyeoman@gmail.com>
2021-05-02 13:50:40 +0800

    Hello World

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,62 +1,93 @@
-import React from 'react'
+import React from "react"
 import { Link } from "gatsby"
-import styled from 'styled-components'
+import styled, { keyframes } from "styled-components"
 import { themes } from "../styles/ColorStyles"
-import { H1, H3, SmallText2 } from '../styles/TextStyles';
-import PurchaseButton from '../buttons/PurchaseButton';
-import MockupAnimation from '../animations/MockupAnimation';
+import { H1, H3, SmallText2 } from "../styles/TextStyles"
+import PurchaseButton from "../buttons/PurchaseButton"
+import MockupAnimation from "../animations/MockupAnimation"
+import WaveBackground from "../backgrounds/WaveBackground"
 
-function HeroSection (){
-    return(
-        <Wrapper>
-         <ContentWrapper>
-                <TextWrapper>
-                    <Title>A.L.I
-                        <SubTitle>Personal Assistant <br /> & Best Friend</SubTitle>
-                    </Title>
-                    <Description>"Good Evening Sir, the weather is 28 and Sunny."</Description>
-                    <PurchaseButton title="Start System" subtitle="Begin Todays Analysis"/>
-                </TextWrapper>
-                <MockupAnimation />
-          </ContentWrapper>
-        </Wrapper>
-    )
+function HeroSection() {
+  return (
+    <Wrapper>
+      <WaveBackground />
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>
+            A.L.I
+            <SubTitle>
+              Personal Assistant <br /> & Best Friend
+            </SubTitle>
+          </Title>
+          <Description>
+            "Good Evening Sir, the weather is 28 and Sunny."
+          </Description>
+          <PurchaseButton
+            title="Start System"
+            subtitle="Begin Todays Analysis"
+          />
+        </TextWrapper>
+        <MockupAnimation />
+      </ContentWrapper>
+    </Wrapper>
+  )
 }
 
+export default HeroSection
 
-export default HeroSection;
+const animation = keyframes`
+    from { opacity: 0; transform: translateY(-10px); filter: blur(10px);}
+    to { opacity: 1;  transform: translateY(0px); filter: blur(0px);}
+`
 
 const Wrapper = styled.div`
-background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
-overflow: hidden;
+  overflow: hidden;
 
-// Can remove in future once more content is added
-height: 100vh;
+  // Can remove in future once more content is added
+  height: 100vh;
 `
 
 const ContentWrapper = styled.div`
-max-width: 1234px;
-margin: 0 auto;
-padding: 200px 30px;
-display: grid;
-grid-template-columns: 360px auto;
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 
 const TextWrapper = styled.div`
-max-width: 360px;
-display: grid;
-gap: 30px;
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+
+  // Select only the immediate children
+  > * {
+    opacity: 0;
+    animation: ${animation} 1s forwards;
+
+    :nth-child(1) {
+      animation-delay: 0s;
+    }
+    :nth-child(2) {
+      animation-delay: 0.2s;
+    }
+    :nth-child(3) {
+      animation-delay: 0.4s;
+    }
+  }
 `
 
 const Title = styled(H1)`
-color: ${themes.dark.text1};
+  color: ${themes.dark.text1};
+  opacity: 0;
+  animation: ${animation} 1s 0.2s forwards;
 `
 
 const SubTitle = styled(H3)`
-text-transform: uppercase;
+  text-transform: uppercase;
 `
 
 const Description = styled(SmallText2)`
-font-size: 17px;
-line-height:130%; 
+  font-size: 17px;
+  line-height: 130%;
 `
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,6 @@ function SecondPage() {
   return (
     <Layout>
       <SEO title="Page two" />
-      <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
       <Link to="/">Go back to the homepage</Link>
     </Layout>
